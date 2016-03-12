0140ff8fa40a508bac50c1628ffcf49d094b3af0
Zoltan Altfatter <altfatterz@gmail.com>
2016-03-12 19:54:04 +0100

    change package name

--- src/test/java/com/zoltanaltfatter/SpringBootFlywayApplicationTests.java
+++ src/test/java/com/zoltanaltfatter/SpringBootFlywayApplicationTests.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
