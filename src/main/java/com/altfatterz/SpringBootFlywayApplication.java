ac272b68766475c43a0de767ad331bae9b657fcb
Zoltan Altfatter <altfatterz@gmail.com>
2016-03-12 19:51:30 +0100

    working single file

--- src/main/java/com/altfatterz/SpringBootFlywayApplication.java
+++ src/main/java/com/altfatterz/SpringBootFlywayApplication.java
@@ -2,6 +2,10 @@ package com.altfatterz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import javax.persistence.*;
 
 @SpringBootApplication
 
@@ -15,4 +19,61 @@ public class SpringBootFlywayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootFlywayApplication.class, args);
 	}
+}
+
+@Entity
+@Table(name = "athletes")
+class Athlete {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String firstName;
+
+	@Column(nullable = false)
+	private String lastName;
+
+	// optional
+	private String country;
+
+	// for JPA to work
+	protected Athlete() {
+	}
+
+	public Athlete(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Athlete[id=%d, firstName='%s', lastName='%s'", id, firstName, lastName);
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+}
+
+@RepositoryRestResource(collectionResourceRel = "athletes", path = "/athletes")
+interface AthleteRepository extends CrudRepository<Athlete, Long> {
+
 }
